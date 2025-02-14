d6a842dd9b85652e4330708efaa3fc1fccd1cf4f
Ty <122072344+tyy-8@users.noreply.github.com>
2025-02-14 17:52:43 +1100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using BepInEx;
+using Photon.Pun;
+using UnityEngine;
+using Utilla;
+using Utilla.Attributes;
+using Utilla.Models;
+
+namespace MonkeGus
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    [ModdedGamemode("monkegus_Infection", "MONKEGUS", BaseGamemode.Infection)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public bool inRoom;
+        [ModdedGamemodeJoin]
+        public void OnJoin()
+        {
+            inRoom = true;
+        }
+        [ModdedGamemodeLeave]
+        public void OnLeave()
+        {
+            inRoom = false;
+        }
+
+        public void Update()
+        {
+            if (inRoom)
+            {
+                foreach (VRRig rig in GorillaParent.instance.vrrigs)
+                {
+                    if (rig != GorillaTagger.Instance.offlineVRRig)
+                    {
+                        Material[] sharedMaterials = rig.mainSkin.sharedMaterials;
+                        sharedMaterials[0] = rig.materialsToChangeTo[0];
+                        sharedMaterials[1] = rig.defaultSkin.chestMaterial;
+                        rig.mainSkin.sharedMaterials = sharedMaterials;
+                        rig.mainSkin.material.color = Color.grey;
+                        rig.concatStringOfCosmeticsAllowed = null;
+                        rig.playerText2.gameObject.SetActive(false);
+                        rig.playerText1.gameObject.SetActive(false);
+                        rig.lavaParticleSystem.Stop();
+                    }
+                }
+            }
+            else
+            {
+                // just in case
+                foreach (VRRig rig in GorillaParent.instance.vrrigs)
+                {
+                    rig.showName = true;
+                }
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MonkeGus
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.ty.gorillatag.monkegus";
+        public const string Name = "MonkeGus";
+        public const string Version = "1.0.0";
+    }
+}
